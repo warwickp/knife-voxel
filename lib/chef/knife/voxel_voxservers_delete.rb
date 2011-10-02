99cac367e458d3745a21308dec84defa395b30ab
James W. Brinkerhoff <jwb@paravolve.net>
2011-10-02 19:33:04 -0400

    0.0.4: VoxSERVER Support (create and delete)

---
+++ lib/chef/knife/voxel_voxservers_delete.rb
@@ -0,0 +1,45 @@
+require 'chef/knife/voxel_base'
+
+class Chef
+  class Knife
+    class VoxelVoxserversDelete < Knife
+      include Knife::VoxelBase
+
+      banner "knife voxel voxservers delete (options)"
+
+      def run
+        @name_args.each do |device_id|
+          device = hapi.voxel_devices_list( :device_id => device_id, :verbosity => 'extended' )
+
+          if device['stat'] == "fail"
+            STDERR.puts "ERROR: #{device['err']['msg']}"
+          else
+            device = device['devices']['device']
+
+            if device['type']['content'] == "Server"
+              puts "#{ui.color("Device ID", :cyan)}: #{device['id']}"
+              puts "#{ui.color("Name", :cyan)}: #{device['label']}"
+              puts "#{ui.color("Image Id", :cyan)}: #{config[:image_id]}"
+              puts "#{ui.color("Facility", :cyan)}: #{device['location']['facility']['code']}"
+              puts "#{ui.color("Public IP Address", :cyan)}: #{device['ipassignments']['ipassignment'].select { |i| i['type'] == 'frontend' }.first['content']}"
+              puts "#{ui.color("Private IP Address", :cyan)}: #{device['ipassignments']['ipassignment'].select { |i| i['type'] == 'backend' }.first['content']}"
+              puts "\n"
+
+              confirm("Do you really want to delete this VoxSERVER device?")
+
+              delete = hapi.voxel_voxservers_delete( :device_id => device_id )
+
+              if delete['stat'] == "ok"
+                ui.warn("Deleted VoxSERVER device #{device['id']} named #{device['label']}")
+              else
+                ui.error("Error removing VoxSERVER device #{device['label']}")
+              end
+            else
+              ui.warn("Device #{device['id']} is not a VoxSERVER device.")
+            end
+          end
+        end
+      end
+    end
+  end
+end
